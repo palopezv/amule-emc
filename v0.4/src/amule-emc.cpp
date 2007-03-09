7207e84c332ba87e74bd583ce16e1a8a02fee729
wuischke <wuischke@a118e30d-e428-0410-b080-f19ffadea53f>
2007-03-09 21:17:44 +0000

    Added binary file structure overview

--- v0.4/src/amule-emc.cpp
+++ v0.4/src/amule-emc.cpp
@@ -26,7 +26,7 @@ int main(int argc, char *argv[])
 {
 	int i;
 
-	for (i = 0; i < argc; i++)
+	for (i = 1; i < argc; i++)
 	{
 		OutputEd2kLinks(argv[i]);
 	}
--- v0.4/src/collection.cpp
+++ v0.4/src/collection.cpp
@@ -23,6 +23,8 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <sstream>
 #include <fstream>
 
+#include <iostream>
+
 collection::collection()
 {
 };
@@ -94,7 +96,84 @@ std::string	collection::GetFileHash( int index )
 
 bool	collection::OpenBinary( std::string File )
 {
+#warning function OpenBinary( std::string File ) is not finished
 	return false;
+	
+	
+	std::ifstream infile;
+	
+	infile.open(File.c_str(), std::ifstream::in|std::ifstream::binary);
+		if(!infile.is_open())
+			return false;
+
+/*	emulecollection file structure:
+
+uint32_t emulecollection version
+values:	COLLECTION_FILE_VERSION1_INITIAL		0x01
+	COLLECTION_FILE_VERSION2_LARGEFILES		0x02
+	
+	version 2 files support files bigger than 4GB (uint64_t file sizes)
+
+uint32_t header tag count
+
+header tags:
+	FT_FILENAME					0x01
+		TAGTYPE_STRING					0x02
+		uint16_t					string length
+		std::string					string value
+	FT_COLLECTIONAUTHOR	 			0x31
+		TAGTYPE_STRING					0x02
+		uint16_t					string length
+		std::string					string value
+	FT_COLLECTIONAUTHORKEY				0x32
+		TAGTYPE_BLOB					0x07
+		uint32_t					blob size
+		(notype)					blob data
+
+uint32_t collection file count
+
+uint32_t file tag count
+
+	at this point the TAGTYPE 0x01 will appear as 0x81 (uType | 0x80)
+	-> see eMule packets.cpp
+	
+	TAGTYPE_HASH					0x01
+		FT_FILEHASH					0x28
+		(notype)[16]					hash data
+
+	TAGTYPE_UINT32					0x03
+		FT_FILESIZE					0x02
+		uint32_t					file size
+	TAGTYPE_UINT16					0x08
+		FT_FILESIZE					0x02
+		uint16_t					file size
+	TAGTYPE_UINT8					0x09
+		FT_FILESIZE					0x02
+		uint8_t					file size
+	TAGTYPE_UINT64					0x0B
+		FT_FILESIZE					0x02
+		uint64_t					file size
+
+	TAGTYPE_STR3					0x13
+		FT_FILENAME					0x01
+		std::string					file name		
+
+(notype) signature (only for signed files)
+		
+*/
+
+
+
+	uint32_t cVersion;
+	infile.read (reinterpret_cast<char *>(&cVersion),sizeof(uint32_t));
+	
+	
+	
+	std::cout << "cVersion: " << cVersion << std::endl;
+	
+	infile.close();
+
+	return true;
 };
 
 bool	collection::OpenText( std::string File )
@@ -104,7 +183,7 @@ bool	collection::OpenText( std::string File )
 	std::ifstream infile;
 	
 	infile.open(File.c_str(), std::ifstream::in);
-		if(!infile)
+		if(!infile.is_open())
 			return false;
   	
   	while (getline(infile, line))
